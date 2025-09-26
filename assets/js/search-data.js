402c6f2fc19805ff86d1b4f66fea66f120e2a31f
GuoliangZou <84260041+GuoliangZou@users.noreply.github.com>
2025-09-26 08:21:13 +0000

    Deploying to gh-pages from @ GuoliangZou/GuoliangZou.github.io@3ae40b5d83d1383aa209c79faa6c99dbdbb8489f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,18 +398,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-one-paper-was-accepted-by-the-35th-aaai-conference-on-artificial-intelligence-aaai-21-virtual-only-conference-feb-2021",
+            },},{id: "news-one-paper-was-accepted-by-the-35th-aaai-conference-on-artificial-intelligence-aaai-21-virtual-only-conference-feb-2021",
           title: 'One paper was accepted by the 35th AAAI Conference on Artificial Intelligence [AAAI-21],...',
           description: "",
           section: "News",},{id: "projects-project-1",
